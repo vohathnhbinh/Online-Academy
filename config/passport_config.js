b60f49e9c607af67ab09f25be7a2cd6708e86f07
VÕ HẠ THANH BÌNH <18120291@student.hcmus.edu.vn>
2020-12-29 19:10:10 +0700

    Add login with email

--- config/passport_config.js
+++ config/passport_config.js
@@ -4,11 +4,17 @@ const User = require('../models/user')
 
 module.exports = passport => {
     const authenticateUser = async (username, password, done) => {
+        const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        let email
+        if(re.test(username)) {
+            email = username
+        }
         try {
-            const user = await User.findOne({
-                username: username
-            })
-            if (!user) return done(null, false, {message: 'Incorrect username'})
+            const user = await User.findOne({$or: [
+                {username: username},
+                {email: email}
+            ]})
+            if (!user) return done(null, false, {message: 'Incorrect username or email'})
             else {
                 const result = await bcrypt.compare(password, user.password)
                 if (result) return done(null, user)
