11fd84d8d3e98ac17195cf44ea63a87aba3df9ac
letrongbang120 <55828168+letrongbang120@users.noreply.github.com>
2021-01-09 14:24:29 +0700

    fix edit button/ add edit course view

--- config/helper.js
+++ config/helper.js
@@ -1,5 +1,8 @@
 module.exports = hbs => {
     hbs.handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
         return (arg1 == arg2) ? options.fn(this) : options.inverse(this)
+    }),
+    hbs.handlebars.registerHelper('ifIdEquals', function(arg1, arg2, options){
+        return (arg1.equals(arg2)) ? options.fn(this) : options.inverse(this)
     })
 }
