1926cf204ad1d334eae628be32ed13a79f2510ae
VÕ HẠ THANH BÌNH <18120291@student.hcmus.edu.vn>
2021-01-15 11:34:07 +0700

    Courses appear differently if new, hot, or sale available

--- config/helper.js
+++ config/helper.js
@@ -12,5 +12,19 @@ module.exports = hbs => {
             return (!arg1.equals(arg2) ) ? options.fn(this) : options.inverse(this)
         } else return options.inverse(this)
     }),
-    hbs.handlebars.registerHelper('dateFormat', require('handlebars-dateformat'))
+    hbs.handlebars.registerHelper('dateFormat', require('handlebars-dateformat')),
+    hbs.handlebars.registerHelper('isNew', function(arg, options) {
+        if(arg) {
+            const today = new Date()
+            const dayDiff = (today.getTime() - arg.getTime()) / (1000 * 60 * 60 * 24)
+            if (dayDiff <= 7) return options.fn(this)
+            return options.inverse(this)
+        } else return options.inverse(this)
+    }),
+    hbs.handlebars.registerHelper('isHot', function(arg, options) {
+        if(arg) {
+            if (arg >= 4.5) return options.fn(this)
+            return options.inverse(this)
+        } else return options.inverse(this)
+    })
 }
